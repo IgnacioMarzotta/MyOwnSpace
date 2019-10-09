9157e8bb9cc4f2547048db195a13636f79e0fcd9
Ignacio Marzotta <marzottanacho@gmail.com>
2019-10-09 13:32:15 -0300

    Modified the seeds file so each new user has notes. Modified some style in navbar, login & sign-up links. Made minor visual changes to index & landing

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,13 +9,6 @@ Note.destroy_all
 Bodycolor.destroy_all
 User.destroy_all
 
-10.times do
-    User.create(
-        username: Faker::Internet.username,
-        email: Faker::Internet.email,
-        password: 123456)
-end
-
 Bodycolor.create(name: 'Yellow', value: 'bg-card-yellow border-top-yellow text-dark')
 Bodycolor.create(name: 'White', value: 'bg-card-white border-top-white text-dark')
 Bodycolor.create(name: 'Grey', value: 'bg-card-grey border-top-grey text-dark')
@@ -25,3 +18,10 @@ Bodycolor.create(name: 'Green', value: 'bg-card-green border-top-green text-dark
 Bodycolor.create(name: 'Orange', value: 'bg-card-orange border-top-orange text-dark')
 Bodycolor.create(name: 'Red', value: 'bg-card-red border-top-red text-dark')
 Bodycolor.create(name: 'Purple', value: 'bg-card-purple border-top-purple text-dark')
+
+10.times do
+    a = User.create(username: Faker::Internet.username, email: Faker::Internet.email, password: 123456, password_confirmation: 123456)
+    9.times do |i|
+        Note.create(user_id: a.id, bodycolor_id: Bodycolor.all.sample.id, title: "Note n°#{i}", description: "Note n°#{i}'s description", content: "This is note n°#{i}'s content, here should be a pretty long lorem ipsum or something like that")
+    end
+end
