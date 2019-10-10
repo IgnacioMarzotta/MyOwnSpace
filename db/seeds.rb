f8f0b9e42263dc3a0ce22504b993bab18fce2a1b
Ignacio Marzotta <marzottanacho@gmail.com>
2019-10-10 15:35:43 -0300

    Made visual & layout changes to notes/index, visual changes to navbar, footer & landing.Seeds file was modified to add colred spans to each note color during selection(not working yet)

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,15 +9,15 @@ Note.destroy_all
 Bodycolor.destroy_all
 User.destroy_all
 
-Bodycolor.create(name: 'Yellow', value: 'bg-card-yellow border-top-yellow text-dark')
-Bodycolor.create(name: 'White', value: 'bg-card-white border-top-white text-dark')
-Bodycolor.create(name: 'Grey', value: 'bg-card-grey border-top-grey text-dark')
-Bodycolor.create(name: 'Black', value: 'bg-card-black border-top-black text-dark')
-Bodycolor.create(name: 'Blue', value: 'bg-card-blue border-top-blue text-dark')
-Bodycolor.create(name: 'Green', value: 'bg-card-green border-top-green text-dark')
-Bodycolor.create(name: 'Orange', value: 'bg-card-orange border-top-orange text-dark')
-Bodycolor.create(name: 'Red', value: 'bg-card-red border-top-red text-dark')
-Bodycolor.create(name: 'Purple', value: 'bg-card-purple border-top-purple text-dark')
+Bodycolor.create(name: '<span class="badge badge-yellow">..</span>Yellow', value: 'bg-card-yellow border-top-yellow text-dark')
+Bodycolor.create(name: '<span class="badge badge-blue"></span> Blue', value: 'bg-card-blue border-top-blue text-dark')
+Bodycolor.create(name: '<i class="badge badge-green">..</i> Green', value: 'bg-card-green border-top-green text-dark')
+Bodycolor.create(name: '<span class="badge badge-white">..</span>White', value: 'bg-card-white border-top-white text-dark')
+Bodycolor.create(name: '<span class="badge badge-grey">..</span>Grey', value: 'bg-card-grey border-top-grey text-dark')
+Bodycolor.create(name: '<span class="badge badge-black">..</span>Black', value: 'bg-card-black border-top-black text-dark')
+Bodycolor.create(name: '<span class="badge badge-orange">..</span>Orange', value: 'bg-card-orange border-top-orange text-dark')
+Bodycolor.create(name: '<span class="badge badge-red">..</span>Red', value: 'bg-card-red border-top-red text-dark')
+Bodycolor.create(name: '<span class="badge badge-purple">..</span>Purple', value: 'bg-card-purple border-top-purple text-dark')
 
 10.times do
     a = User.create(username: Faker::Internet.username, email: Faker::Internet.email, password: 123456, password_confirmation: 123456)
