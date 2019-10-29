5d91ca2bc51578253c9520db52aa9fdbde813a89
Ignacio Marzotta <marzottanacho@gmail.com>
2019-10-29 10:34:25 -0300

    Added style to Login pages. Added extra buttons and functionality to calendar

--- app/models/note.rb
+++ app/models/note.rb
@@ -2,7 +2,7 @@ class Note < ApplicationRecord
   belongs_to :user
   belongs_to :bodycolor
   has_rich_text :content
-  has_one_attached :image
+  has_many_attached :images
   validates :title, length: {maximum: 64}
   validates :description, length: {maximum: 128}
   validates :content, length: {maximum: 10000}
